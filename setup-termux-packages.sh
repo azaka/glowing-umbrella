01a658e36df7e4b6d9bb5de3637ef433ae1f0293
root <email>
2017-05-02 11:50:35 +0000

    finish the setup sctipt

--- setup-termux-packages.sh
+++ setup-termux-packages.sh
@@ -4,4 +4,9 @@ add-apt-repository ppa:jonathonf/python-3.6
 apt-get update
 pushd ..
 git clone https://github.com/termux/termux-packages
-
+pushd termux-packages
+sed -i 's/sudo//g' scripts/setup-ubuntu.sh
+./scripts/setup-ubuntu.sh
+./scripts/setup-android-sdk.sh
+popd
+popd
