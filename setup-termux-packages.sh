7dd54f4b727cd168b2c9370046ce642005fcc33f
root <email>
2017-05-02 10:57:41 +0000

    track setup-termux-packages.sh

---
+++ setup-termux-packages.sh
@@ -0,0 +1,7 @@
+apt-get update
+apt-get install git nano software-properties-common -y
+add-apt-repository ppa:jonathonf/python-3.6
+apt-get update
+pushd ..
+git clone https://github.com/termux/termux-packages
+
